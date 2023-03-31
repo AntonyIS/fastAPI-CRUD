703cca3b6165c6787bf419c9bffc6165c31d989e
AntonyIS <antonyshikubu@gmail.com>
2023-03-31 13:02:38 +0300

    Adding form data processing

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 from typing import Union
-from fastapi import FastAPI
-from model import Book, session
+import uuid
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
 
-# Initialize FastAPI App
+templates = Jinja2Templates(directory="templates/")
+
 app = FastAPI()
+users = []
 
-@app.get("/books")
-def read_books():
-    book = Book()
-    session.add(book)
-    session.commit()
-
-    return {"books" : "read books"}, 200
+@app.get("/users")
+def read_users(request: Request):
+    return templates.TemplateResponse('home.html', context={'request': request,'users': users})
 
 
-@app.get("/books")
-def post_books():
-    book_query = session.query(Book)
-    return book_query.all(), 200
+@app.post("/users")
+async def post_users(request:Request):
+    form = await request.form()
+    email = form.get("email")
+    password = form.get("password")
+    user_id = str(uuid.uuid4())
+    user = {"email": email, "password": password, "user_id": user_id}
+    users.append(user)
+    return templates.TemplateResponse('home.html', context={'request': request,'users': users})
     
+@app.get("/users/{user_id}")
+async def read_user(request:Request, user_id:str):
+   
+    for user in users:
+       
+        if user['user_id'] == user_id :
+            
+            return templates.TemplateResponse('user.html', context={'request': request,'user': user})
+        else:
+            return templates.TemplateResponse('user.html', context={'request': request,'user': {}})
 
-
-@app.get("/books/{book_id}")
-def read_book(book_id:int, q:Union[str, None] = None):
+@app.put("/users/{user_id}")
+def update_user(user_id:int, q:Union[str, None] = None):
     return {
-        "book" : f"Read book with id: {book_id}"
+        "user" : f"user with id: {user_id}"
     }
 
-@app.put("/books/{book_id}")
-def update_book(book_id:int, q:Union[str, None] = None):
+@app.delete("/users/{user_id}")
+def delete_user(user_id:int, q:Union[str, None] = None):
     return {
-        "book" : f"book with id: {book_id}"
-    }
-
-@app.delete("/books/{book_id}")
-def delete_book(book_id:int, q:Union[str, None] = None):
-    return {
-        "book" : f"book with id: {book_id}"
+        "user" : f"user with id: {user_id}"
     }
